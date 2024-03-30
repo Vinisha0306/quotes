0612d7f057166bf46e355f97f7d4d4bb436506e5
Aneri09 <aneridobariya9@gmail.com>
2024-03-30 19:51:22 +0530

    Edited

--- lib/pages/edit_page/edit_page.dart
+++ lib/pages/edit_page/edit_page.dart
@@ -10,6 +10,10 @@ class EditPage extends StatefulWidget {
   State<EditPage> createState() => _EditPageState();
 }
 
+Color BGC = Colors.white;
+Color TC = Colors.black;
+double opacity = 1;
+
 class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
@@ -36,12 +40,159 @@ class _EditPageState extends State<EditPage> {
         ),
         backgroundColor: Colors.grey.shade200,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      BGC = Colors.white;
+                      TC = Colors.black;
+                      opacity = 1;
+                      setState(() {});
+                    },
+                    child: const Text(
+                      'Reset',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Save',
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  color: BGC.withOpacity(opacity),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      quote.quote,
+                      style: TextStyle(
+                        fontSize: 20,
+                        wordSpacing: 10,
+                        color: TC,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        "- ${quote.author}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: TC,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Background Color',
+                style: TextStyle(fontSize: 18),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      Colors.primaries.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: GestureDetector(
+                          onTap: () {
+                            BGC = Colors.primaries[index];
+                            setState(() {});
+                          },
+                          child: CircleAvatar(
+                            backgroundColor: Colors.primaries[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Opacity(Shade)',
+                style: TextStyle(fontSize: 18),
+              ),
+              Slider(
+                value: opacity,
+                min: 0,
+                max: 1,
+                onChanged: (val) {
+                  opacity = val;
+                  setState(() {});
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Text Color',
+                style: TextStyle(fontSize: 18),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      Colors.primaries.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: GestureDetector(
+                          onTap: () {
+                            TC = Colors.primaries[index];
+                            setState(() {});
+                          },
+                          child: CircleAvatar(
+                            backgroundColor: Colors.primaries[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
