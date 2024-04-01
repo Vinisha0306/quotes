46673fefcd183757230920b1af4a3b68cf15f5a9
Aneri09 <aneridobariya9@gmail.com>
2024-04-01 17:09:46 +0530

    Edited

--- lib/pages/edit_page/edit_page.dart
+++ lib/pages/edit_page/edit_page.dart
@@ -1,13 +1,44 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:quotes/headers.dart';
-import 'package:quotes/pages/app_bar.dart';
+import 'package:share_extend/share_extend.dart';
 
 import '../../modals/quote_modal.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'dart:ui' as ui;
 
 class EditPage extends StatefulWidget {
   const EditPage({super.key});
 
   @override
   State<EditPage> createState() => _EditPageState();
+}
+
+GlobalKey widgetKey = GlobalKey();
+
+Future<File> getFileFromWidget() async {
+  RenderRepaintBoundary boundary =
+      widgetKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+  ui.Image image = await boundary.toImage(
+    pixelRatio: 15,
+  );
+  ByteData? data = await image.toByteData();
+  Uint8List list = data!.buffer.asUint8List();
+
+  Directory directory = await getTemporaryDirectory();
+  File file = await File(
+          "${directory.path}/QA${DateTime.now().millisecondsSinceEpoch}.png")
+      .create();
+  file.writeAsBytesSync(list);
+
+  return file;
 }
 
 Color BGC = Colors.white;
@@ -64,46 +95,71 @@ class _EditPageState extends State<EditPage> {
                     width: 10,
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      ImageGallerySaver.saveFile(
+                              (await getFileFromWidget()).path,
+                              isReturnPathOfIOS: true)
+                          .then(
+                        (value) => (value) =>
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Saved to gallery !!"),
+                              ),
+                            ),
+                      );
+                    },
                     child: const Text(
                       'Save',
                     ),
                   ),
+                  IconButton(
+                    onPressed: () async {
+                      ShareExtend.share(
+                          (await getFileFromWidget()).path, 'image');
+                    },
+                    icon: Icon(Icons.share),
+                  ),
                 ],
               ),
-              Container(
-                padding: const EdgeInsets.all(40),
-                decoration: BoxDecoration(
-                  color: BGC.withOpacity(opacity),
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              RepaintBoundary(
+                key: widgetKey,
+                child: Container(
+                  padding: const EdgeInsets.all(40),
+                  decoration: BoxDecoration(
+                    color: BGC.withOpacity(opacity),
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      quote.quote,
-                      style: TextStyle(
-                        fontSize: 20,
-                        wordSpacing: 10,
-                        color: TC,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "- ${quote.author}",
+                  child: Column(
+                    children: [
+                      Text(
+                        quote.quote,
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 20,
+                          wordSpacing: 10,
                           color: TC,
                         ),
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          "- ${quote.author}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: TC,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(
